60874667142b6d7441ca2dde6f4c1672ddecce89
Grigori Harutyunyan <grigori.evistem@gmail.com>
2021-09-03 17:08:47 +0400

    AdminMiddleware change

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -18,10 +18,10 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-//        if(Auth::check() && Auth::user()->is_admin){
-//            return $next($request);
-//        }
-//        abort(404);
+        if(Auth::check() && Auth::user()->is_admin){
+            return $next($request);
+        }
+        abort(404);
 
     }
 }
